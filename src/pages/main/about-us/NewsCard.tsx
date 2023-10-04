52608349384b40d120435d047e485c8152983d72
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-04 18:39:03 +0300

    added PreviewBlock and AboutUs block

---
+++ src/pages/main/about-us/NewsCard.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+
+type TNewsCard = {
+    title: string;
+    image: string;
+    link: string;
+}
+
+function NewsCard({title, image, link}: TNewsCard) {
+    return (
+        <Link href={link}>
+            <div className="flex flex-col justify-between h-[370px] w-full max-w-[262px] rounded-5 box-border bg-white">
+                <Image className="object-contain rounded-5 overflow-hidden" src={image} alt={title} width={262} height={276} />
+
+                <div className="w-full p-[25px] box-border">
+                    <p className="text-20 text-black">
+                        {title}
+                    </p>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default NewsCard;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,32 +1,14 @@
 'use strict'
 
-import Image from 'next/image'
-import Tag from "@/components/tag";
-
-const LIST_OF_TAGS = ['inst', 'youtube', 'tenchat', 'tg'];
+import PreviewBlock from "@/pages/main/preview-block/PreviewBlock";
+import AboutUs from "@/pages/main/about-us/AboutUs";
 
 export default function Main() {
     return (
-        <div className="w-full flex h-[calc(100vh-140px)] flex-col items-center justify-end p-[50px] box-border">
-            <div className="w-full flex justify-between h-[210px]">
-                <div className="max-w-[560px]">
-                    <h1 className="text-white text-heading-l">
-                        Путь в тысячу миль начинается с одного шага
-                    </h1>
-                </div>
-                <div className="flex justify-between flex-col">
-                    <div className="flex relative gap-[10px]">
-                        {
-                            LIST_OF_TAGS.map((title) => (
-                                <Tag title={title} key={title} />
-                            ))
-                        }
-                    </div>
-                    <p className="text-20 max-w-[400px]">
-                        Команда Way11 поможет вам реализовать IT‑решения для любого бизнеса. В основе наших проектов лежат идеи, основанные на аналитике, креативе и технологиях.
-                    </p>
-                </div>
-            </div>
+        <div className="w-full flex flex-col items-center">
+
+            <PreviewBlock />
+            <AboutUs />
         </div>
     )
 }
