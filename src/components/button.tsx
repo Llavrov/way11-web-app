c7a291e080d553fe24154a9fec5066d0b7ab921a
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-04 13:53:43 +0300

    infrastructure

---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+'use strict';
+
+export default function Button() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+        </main>
+    )
+}
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,11 @@
+'use strict'
+
+import Image from 'next/image'
+
+export default function Main() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+
+        </div>
+    )
+}
