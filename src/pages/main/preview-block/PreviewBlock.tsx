e1b0b8e15eee3f76cd8e7af7ccb2922c26be7b3e
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-05 17:39:07 +0300

    main: header, about-us block, preview-block, bg anim

--- src/pages/main/preview-block/PreviewBlock.tsx
+++ src/pages/main/preview-block/PreviewBlock.tsx
@@ -1,13 +1,30 @@
 'use strict'
 
-import Image from 'next/image'
 import Tag from "@/components/tag";
+import {motion} from "framer-motion";
+import Background01 from "@/pages/main/background";
+import Background02 from "@/pages/main/background02";
 
 const LIST_OF_TAGS = ['inst', 'youtube', 'tenchat', 'tg'];
 
+const svgVariants = {
+    hidden: { y: '50vh', x: '0vw', opacity: 0, },
+    visible: {
+        y: '-80vh', x: '-40vw', opacity: 1,
+        transition: { duration: 3 }
+    },
+}
+const svgVariantsBig = {
+    visible: {
+        y: ['-45vh', '-35vh'], x: '20vw', scale: [1, 1], rotate: 0,
+        transition: { duration: 3 },
+    },
+    hidden: { y: '20vh', x: '-150vw', scale: 1, rotate: -180 },
+};
+
 export default function PreviewBlock() {
     return (
-        <div className="w-full flex h-[calc(100vh-140px)] flex-col items-center justify-end p-[50px] box-border">
+        <div className="w-full flex h-screen relative flex-col items-center justify-end p-[50px] box-border">
             <div className="w-full flex justify-between h-[210px]">
                 <div className="max-w-[560px]">
                     <h1 className="text-white text-heading-l">
@@ -27,6 +44,34 @@ export default function PreviewBlock() {
                     </p>
                 </div>
             </div>
+
+            <motion.div style={{ position: 'absolute', top: 0, right: 0, zIndex: -1 }}>
+                <motion.svg
+                    initial='hidden'
+                    variants={svgVariantsBig}
+                    animate='visible'
+                    width="1438"
+                    height="1324"
+                    viewBox="0 0 1438 1324"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <Background01 />
+                </motion.svg>
+                <motion.svg
+                    className='w-full'
+                    initial='hidden'
+                    variants={svgVariants}
+                    animate='visible'
+                    width="218"
+                    height="609"
+                    viewBox="0 0 218 609"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <Background02 />
+                </motion.svg>
+            </motion.div>
         </div>
     )
 }
