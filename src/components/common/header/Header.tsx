1588ef5cb91d78a1be5ecff5da606bbdb78a91bb
PaPasha00 <vasev-2020@bk.ru>
2023-10-14 18:58:32 +0300

    add menu burger anumation

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Button from "@/components/common/buttons/button";
 import { useState } from "react";
 import HeaderMenu from "./headerMenu";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 const TABS = [
     {
@@ -18,27 +18,40 @@ const TABS = [
     }
 ]
 
-const fadeIdVariants = {
-    hidden: {
-        opacity: 0,
-    },
-    animationThree: {
-        opacity: 1,
-        transition: {
-            duration: 1.2,
-        }
-    },
-    exit: {
-        opacity: 0
-    }
+const path01Variants = {
+    open: { d: 'M3.06061 2.99999L21.0606 21'},
+    closed: { d: 'M0 9.5L24 9.5'},
+}
+const path02Variants = {
+    open: { d: 'M3.00006 21.0607L21 3.06064'},
+    closed: { d: 'M0 14.5L24 14.5'},
+}
+const path03Variants = {
+    open: { d: 'M0 0L0 0', opacity: 0},
+    closed: { d: 'M0 19.5L24 19.5', opacity: 1},
 }
 
 export default function Header() {
     const [openMenu, setOpenMenu] = useState(false);
+    const [isOpen, setOpen] = useState(false);
+    const path01Controls = useAnimation();
+    const path02Controls = useAnimation();
+    const path03Controls = useAnimation();
 
-    const changeMenuState = (e: boolean) => {
+    const changeMenuState = async (e: boolean) => {
+        setOpen(!isOpen);
         setOpenMenu(e)
-    }
+
+        if (!isOpen) {
+            path01Controls.start(path01Variants.open);
+            path02Controls.start(path02Variants.open);
+            path03Controls.start(path03Variants.open);
+        } else {
+            path01Controls.start(path01Variants.closed);
+            path02Controls.start(path02Variants.closed);
+            path03Controls.start(path03Variants.closed);
+        }
+    };
 
     return (
         <>
@@ -74,19 +87,31 @@ export default function Header() {
                 <button
                     onClick={() => changeMenuState(!openMenu)}
                     className="hidden h-[90px] flex justify-center items-center pr-[30px] lg:flex">
-                    {
-                        openMenu
-                            ? <Image
-                                width={40}
-                                height={20}
-                                src='/svg/closeMenu.svg'
-                                alt="menu" />
-                            : <Image
-                                width={40}
-                                height={20}
-                                src='/svg/menuButton.svg'
-                                alt="menu" />
-                    }
+
+                    <svg width='24' height='24' viewBox='0 0 24 24'>
+                        <motion.path
+                            {...path01Variants.closed}
+                            animate={path01Controls}
+                            transition={{ duration: 0.4 }}
+                            stroke='#FFFFFF'
+                            strokeWidth='2'
+                        />
+                        <motion.path
+                            {...path02Variants.closed}
+                            animate={path02Controls}
+                            transition={{ duration: 0.4 }}
+                            stroke='#FFFFFF'
+                            strokeWidth='2'
+                        />
+                        <motion.path
+                            {...path03Variants.closed}
+                            animate={path03Controls}
+                            transition={{ duration: 0.4 }}
+                            stroke='#FFFFFF'
+                            strokeWidth='2'
+                        />
+                    </svg>
+
                 </button>
             </header>
 
