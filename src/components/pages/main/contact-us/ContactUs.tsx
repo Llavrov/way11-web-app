eeb688a79fe56002b40eebe948061f27495a457a
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-18 22:23:52 +0300

    contact us

--- src/components/pages/main/contact-us/ContactUs.tsx
+++ src/components/pages/main/contact-us/ContactUs.tsx
@@ -5,17 +5,26 @@ import QUMultiLineTextField from "@/components/common/textFields/QUMultiLineText
 import QUTextField from "@/components/common/textFields/QUTextField";
 import EmptyButton from "@/components/common/buttons/emptyButton";
 import FilePickerComponent from "../../../common/filePickerComponent";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 export default function ContactUs() {
     return (
         <div id="contact-us" className="w-full flex relative flex-row lg:flex-col box-border justify-between rounded-[50px] px-[50px] lg:px-4">
-            <div className="flex gap-2 flex-col items-left max-w-[470px] lg:max-w-[230px]">
-                <div className="flex">
-                    <TagGray title="обратная связь" />
+            <div className="flex gap-2 relative flex-col items-left justify-between max-w-[470px] lg:max-w-[230px]">
+                <div className="w-full">
+                    <div className="flex">
+                        <TagGray title="обратная связь" />
+                    </div>
+                    <p className="text-heading-l text-white lg:text-3xl lg:leading-9">
+                        предложите свой проект
+                    </p>
                 </div>
-                <p className="text-heading-l text-white lg:text-3xl lg:leading-9">
-                    предложите свой проект
-                </p>
+
+                <Link href={'#preview'}>
+                    <Image src={"/scroll-up.svg"} alt="image picker" width={104} height={104} />
+                </Link>
             </div>
 
             <div className="w-[790px] lg:w-full flex flex-col overflow-x-scroll gap-[10px] pt-[64px] lg:pt-[24px]">
