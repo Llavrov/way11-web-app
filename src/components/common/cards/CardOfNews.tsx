78f966a59512f847f10e18deaa846f74bc345c2f
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-28 09:58:47 +0300

    image

--- src/components/common/cards/CardOfNews.tsx
+++ src/components/common/cards/CardOfNews.tsx
@@ -13,7 +13,7 @@ export default function CardOfNews({title, image, link}: TNewsCard) {
     return (
         <Link href={link}>
             <div className="flex flex-col overflow-hidden justify-between h-[463px] lg:h-[367px] w-[390px] min-w-[390px] max-w-[390px] lg:min-w-[262px] lg:max-w-[262px] rounded-5 box-border bg-white">
-                <Image className="object-cover object-[-100px] w-[calc(100%+100px)] h-full max-w-[390px] max-h-[390px] rounded-5 overflow-hidden" src={image} alt={title} width={390} height={350} />
+                <Image className="object-cover w-[calc(100%+100px)] h-full max-w-[390px] max-h-[390px] rounded-5 overflow-hidden" src={image} alt={title} width={390} height={350} />
 
                 <div className="flex justify-center items-center w-full p-auto px-6 lg:px-3 h-[80px] box-border">
                     <p className="text-20 text-black lg:text-text-sm-bold">
